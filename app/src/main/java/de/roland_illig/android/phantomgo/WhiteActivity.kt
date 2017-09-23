beb3bdc30582e0ed50562d9b9409d3a62afab8e2
Roland Illig <roland.illig@gmx.de>
2017-09-23 16:22:56 +0200

    Both Black and White can play

--- app/src/main/java/de/roland_illig/android/phantomgo/WhiteActivity.kt
+++ app/src/main/java/de/roland_illig/android/phantomgo/WhiteActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import de.roland_illig.phantomgo.Player
+import de.roland_illig.phantomgo.PlayerBoardView
 
 class WhiteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_white)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        findViewById<PlayerBoardView>(R.id.playerBoardView)
+                .configure(GameState.GLOBAL.refereeBoard, GameState.GLOBAL.whiteBoard, Player.WHITE)
     }
 
     fun onHandOverClick(view: View) {
