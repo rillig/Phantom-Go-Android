1d483be71659c1064c8309b60f06deb10cabf696
Roland Illig <roland.illig@gmx.de>
2017-10-07 22:09:27 +0200

    Kotlin allows inner functions with convenient syntax, yeah

--- app/src/main/java/de/roland_illig/android/phantomgo/AbstractBoardView.kt
+++ app/src/main/java/de/roland_illig/android/phantomgo/AbstractBoardView.kt
@@ -29,6 +29,8 @@ abstract class AbstractBoardView : View {
 
     private fun init() {
         setOnTouchListener { view, e ->
+            val size = Math.min(width, height)
+            fun screenToBoard(sc: Double) = Math.round(sc * (boardSize + 1) / size - 1).toInt()
             val x = screenToBoard(e.x.toDouble())
             val y = screenToBoard(e.y.toDouble())
             if (x != lastX || y != lastY) {
@@ -48,30 +50,51 @@ abstract class AbstractBoardView : View {
     override fun onDraw(g: Canvas) {
         super.onDraw(g)
 
-        val bsize = boardSize
+        val boardSize = boardSize
+        val screenSize = Math.min(width, height).toFloat()
 
-        val linePaint = linePaint(0xFF000000)
-        val currentLinePaint = linePaint(0xFFFF9900)
+        fun boardToScreen(bc: Double) = Math.round(screenSize * (bc + 1) / (boardSize + 1)).toFloat()
+
+        val lineWidth = Math.max(1.0, Math.floor((boardToScreen(1.0) - boardToScreen(0.0)) / 20.0)).toFloat()
+
+        fun solidPaint(color: Long): Paint {
+            val paint = Paint()
+            paint.color = color.toInt()
+            paint.style = Paint.Style.FILL
+            paint.isAntiAlias = true
+            return paint
+        }
+
+        val linePaint = solidPaint(0xFF000000).also { it.strokeWidth = lineWidth }
+        val currentLinePaint = solidPaint(0xFFFF9900).also { it.strokeWidth = lineWidth }
         val boardPaint = solidPaint(0xFFD48E00)
         val blackPaint = solidPaint(0xFF000000)
         val whitePaint = solidPaint(0xFFFFFFFF)
         val blackTranslucentPaint = solidPaint(0x55000000)
         val whiteTranslucentPaint = solidPaint(0x55FFFFFF)
 
-        g.drawPaint(boardPaint)
+        g.drawRect(RectF(0.toFloat(), 0.toFloat(), screenSize, screenSize), boardPaint)
 
-        for (i in 0 until bsize) {
+        for (i in 0 until boardSize) {
             val start = boardToScreen(0.0)
-            val end = boardToScreen((bsize - 1).toDouble())
+            val end = boardToScreen((boardSize - 1).toDouble())
             val fixed = boardToScreen(i.toDouble())
-            val hpaint = if (i == lastY && lastX in 0 until bsize) currentLinePaint else linePaint
-            val vpaint = if (i == lastX && lastY in 0 until bsize) currentLinePaint else linePaint
+            val hpaint = if (i == lastY && lastX in 0 until boardSize) currentLinePaint else linePaint
+            val vpaint = if (i == lastX && lastY in 0 until boardSize) currentLinePaint else linePaint
             g.drawLine(start, fixed, end, fixed, hpaint)
             g.drawLine(fixed, start, fixed, end, vpaint)
         }
 
-        for (y in 0 until bsize) {
-            for (x in 0 until bsize) {
+        fun fillCircle(g: Canvas, x: Int, y: Int, radius: Double, paint: Paint) {
+            val top = boardToScreen(y - radius)
+            val left = boardToScreen(x - radius)
+            val bottom = boardToScreen(y + radius)
+            val right = boardToScreen(x + radius)
+            g.drawOval(RectF(left, top, right, bottom), paint)
+        }
+
+        for (y in 0 until boardSize) {
+            for (x in 0 until boardSize) {
 
                 val cell = getBoard(x, y)
                 if (cell.dead || cell.territory != null) {
@@ -86,40 +109,6 @@ abstract class AbstractBoardView : View {
                 }
             }
         }
-    }
-
-    private fun fillCircle(g: Canvas, x: Int, y: Int, radius: Double, paint: Paint) {
-        val top = boardToScreen(y - radius)
-        val left = boardToScreen(x - radius)
-        val bottom = boardToScreen(y + radius)
-        val right = boardToScreen(x + radius)
-        g.drawOval(RectF(left, top, right, bottom), paint)
-    }
-
-    private fun lineWidth() = Math.max(1.0, Math.floor((boardToScreen(1.0) - boardToScreen(0.0)) / 20.0))
-
-    private fun boardToScreen(bc: Double): Float {
-        val size = Math.min(width, height)
-        return Math.round((size * bc + size) / (boardSize + 1)).toInt().toFloat()
-    }
-
-    private fun screenToBoard(sc: Double): Int {
-        val size = Math.min(width, height)
-        return Math.round(sc * (boardSize + 1) / size - 1).toInt()
-    }
-
-    private fun linePaint(color: Long): Paint {
-        val paint = solidPaint(color)
-        paint.strokeWidth = lineWidth().toFloat()
-        return paint
-    }
-
-    private fun solidPaint(color: Long): Paint {
-        val paint = Paint()
-        paint.color = color.toInt()
-        paint.style = Paint.Style.FILL
-        paint.isAntiAlias = true
-        return paint
     }
 
     protected class Cell(val color: Player?, val territory: Player?, val dead: Boolean)
