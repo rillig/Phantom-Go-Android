3b3a056d20c67608202a5b92dd5f2028ea9f33d7
Roland Illig <roland.illig@gmx.de>
2020-01-18 15:08:05 +0100

    Use singleton objects instead of companion objects

--- app/src/main/java/de/roland_illig/android/phantomgo/Persistence.kt
+++ app/src/main/java/de/roland_illig/android/phantomgo/Persistence.kt
@@ -5,25 +5,23 @@ import de.roland_illig.phantomgo.Game
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
-class Persistence {
-    companion object {
-        fun load(ctx: Context): Game {
-            try {
-                ctx.openFileInput("state").use {
-                    ObjectInputStream(it).use {
-                        return it.readObject() as Game
-                    }
+object Persistence {
+    fun load(ctx: Context): Game {
+        try {
+            ctx.openFileInput("state").use { file ->
+                ObjectInputStream(file).use { stream ->
+                    return stream.readObject() as Game
                 }
-            } catch (e: Exception) {
-                return Game()
             }
+        } catch (e: Exception) {
+            return Game()
         }
+    }
 
-        fun save(ctx: Context, state: Game) {
-            ctx.openFileOutput("state", Context.MODE_PRIVATE).use {
-                ObjectOutputStream(it).use {
-                    it.writeObject(state)
-                }
+    fun save(ctx: Context, state: Game) {
+        ctx.openFileOutput("state", Context.MODE_PRIVATE).use { file ->
+            ObjectOutputStream(file).use { stream ->
+                stream.writeObject(state)
             }
         }
     }
--- app/src/main/java/de/roland_illig/phantomgo/Referee.kt
+++ app/src/main/java/de/roland_illig/phantomgo/Referee.kt
@@ -3,55 +3,52 @@ package de.roland_illig.phantomgo
 import android.content.res.Resources
 import de.roland_illig.android.phantomgo.R
 
-class Referee {
-    companion object {
-
-        fun comment(result: RefereeResult, color: Player, resources: Resources): String {
-            when (result) {
-                is RefereeResult.OtherStone -> return resources.getString(R.string.referee_other_stone)
-                is RefereeResult.OwnStone -> return resources.getString(R.string.referee_own_stone)
-                is RefereeResult.Suicide -> return resources.getString(R.string.referee_suicide)
-                is RefereeResult.Ko -> return resources.getString(R.string.referee_ko)
-            }
-
-            val black = resources.getString(R.string.referee_black)
-            val white = resources.getString(R.string.referee_white)
-            val isBlack = color == Player.BLACK
-            val me = if (isBlack) black else white
-            val other = if (isBlack) white else black
-
-            if (result is RefereeResult.Pass) {
-                return resources.getString(R.string.referee_passes, me)
-            }
-
-            result as RefereeResult.Ok
-            val atari = result.atari
-            val selfAtari = result.selfAtari
-            val captured = result.capturedStones.size
-
-            if (captured > 0 && selfAtari && atari) {
-                return resources.getQuantityString(R.plurals.referee_capture_atari_selfatari, captured, me, captured, other)
-            }
-            if (captured > 0 && selfAtari) {
-                return resources.getQuantityString(R.plurals.referee_capture_selfatari, captured, me, captured)
-            }
-            if (captured > 0 && atari) {
-                return resources.getQuantityString(R.plurals.referee_capture_atari, captured, me, captured, other)
-            }
-            if (captured > 0) {
-                return resources.getQuantityString(R.plurals.referee_capture, captured, me, captured, other)
-            }
-
-            if (selfAtari && atari) {
-                return resources.getString(R.string.referee_atari_selfatari, me, other)
-            }
-            if (selfAtari) {
-                return resources.getString(R.string.referee_selfatari, me)
-            }
-            if (atari) {
-                return resources.getString(R.string.referee_atari, me, other)
-            }
-            return resources.getString(R.string.referee_to_play, me, other)
+object Referee {
+    fun comment(result: RefereeResult, color: Player, resources: Resources): String {
+        when (result) {
+            is RefereeResult.OtherStone -> return resources.getString(R.string.referee_other_stone)
+            is RefereeResult.OwnStone -> return resources.getString(R.string.referee_own_stone)
+            is RefereeResult.Suicide -> return resources.getString(R.string.referee_suicide)
+            is RefereeResult.Ko -> return resources.getString(R.string.referee_ko)
         }
+
+        val black = resources.getString(R.string.referee_black)
+        val white = resources.getString(R.string.referee_white)
+        val isBlack = color == Player.BLACK
+        val me = if (isBlack) black else white
+        val other = if (isBlack) white else black
+
+        if (result is RefereeResult.Pass) {
+            return resources.getString(R.string.referee_passes, me)
+        }
+
+        result as RefereeResult.Ok
+        val atari = result.atari
+        val selfAtari = result.selfAtari
+        val captured = result.capturedStones.size
+
+        if (captured > 0 && selfAtari && atari) {
+            return resources.getQuantityString(R.plurals.referee_capture_atari_selfatari, captured, me, captured, other)
+        }
+        if (captured > 0 && selfAtari) {
+            return resources.getQuantityString(R.plurals.referee_capture_selfatari, captured, me, captured)
+        }
+        if (captured > 0 && atari) {
+            return resources.getQuantityString(R.plurals.referee_capture_atari, captured, me, captured, other)
+        }
+        if (captured > 0) {
+            return resources.getQuantityString(R.plurals.referee_capture, captured, me, captured, other)
+        }
+
+        if (selfAtari && atari) {
+            return resources.getString(R.string.referee_atari_selfatari, me, other)
+        }
+        if (selfAtari) {
+            return resources.getString(R.string.referee_selfatari, me)
+        }
+        if (atari) {
+            return resources.getString(R.string.referee_atari, me, other)
+        }
+        return resources.getString(R.string.referee_to_play, me, other)
     }
 }
